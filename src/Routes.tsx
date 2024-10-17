94924050af638d1c6a19171156e73f8222aa91f5
SUBIN KHADER B S <subinkhaderbs@SUBINs-MacBook-Air.local>
2024-10-17 15:11:48 +0530

    login

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes as ReactRoutes, Route } from 'react-router-dom'
 import { FormElements } from './pages'
 import { Registration } from './pages/bussinessUsers/Registration'
+import { Login } from './pages/bussinessLogin/login'
 
 export const Routes = () => {
   return (
@@ -9,6 +10,8 @@ export const Routes = () => {
        <ReactRoutes>
         <Route path='/form-element' element={<FormElements/>}></Route>
         <Route path='/registration' element = {<Registration/>}></Route>
+        <Route path='/login' element = {<Login/>}></Route>
+
        </ReactRoutes>
     </div>
   )
